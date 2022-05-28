0d656fb6bcd98e636230ba7612787b6de76fd25a
LucioStedile <https://github.com/LucioStedile>
2022-05-28 15:24:48 -0300

    Atualização

--- app/src/main/java/com/stedile/listadecompras_v1_0/MainActivity.java
+++ app/src/main/java/com/stedile/listadecompras_v1_0/MainActivity.java
@@ -1,14 +1,25 @@
 package com.stedile.listadecompras_v1_0;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.stedile.listadecompras_v1_0.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //setContentView(R.layout.activity_main);
+        //TextView tvHello = findViewById(R.id.tvHello); Forma antiga de ser feito o find
+
+        //Nova forma usando o viewBinding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
