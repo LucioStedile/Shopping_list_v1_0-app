8f8512404ba01dc63c0a6392785c668955f24351
LucioStedile <https://github.com/LucioStedile>
2022-05-28 12:12:38 -0300

    first commit

---
+++ app/src/main/java/com/stedile/listadecompras_v1_0/MainActivity.java
@@ -0,0 +1,14 @@
+package com.stedile.listadecompras_v1_0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
